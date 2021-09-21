57d48a407a5a0d2a71692b5b7c911fa1e07d39c1
Navvya21 <75784090+Navvya21@users.noreply.github.com>
2021-09-21 18:21:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,45 @@
-function startClassification()
-navigator.mediaDevices.getSupportedConstraints({audio : true});
-classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/Wi-8y4N9Y/.json', modelReady)
+function startClassification() {
+    navigator.mediaDevices.getSupportedConstraints({
+        audio: true
+    });
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/Inlbz8Vay/model.json', modelReady)
+}
 
-function modelReady(){
-    classifier.classifiy(gotResults);
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+var cat=0;
+var dog=0;
+var lion= 0;
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+        document.getElementById("result_label").innerHTML = "Detected voice is of - " + results[0].label;
+        document.getElementById("result_count").innerHTML = "Detected Dog - " + dog + " Detected Cat - " + cat + "Detected Lion - " + lion;
+        document.getElementById("result_label").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
+        document.getElementById("result_count").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
+
+        img = document.getElementById('animal_image');
+        if (results[0].label == "Barking") {
+            img.src = 'dog1.png';
+            dog = dog + 1;
+        } else if (results[0].label == "Meowing") {
+            img.src = 'cat.gif';
+            cat = cat + 1;
+
+        } else if (results[0].label == "Roaring") {
+            img.src = 'lion.gif';
+            lion = lion + 1;
+        }else {
+            img.src = 'listen.gif';
+        }
+
+    }
 }
